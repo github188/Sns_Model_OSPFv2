0a773f473098577229ae0137ce7053be87f4058a
jojo59516 <18510510879@163.com>
2016-04-27 16:17:34 +0800

    新建的工程

---
+++ Sim_Model/Sim_Model.cpp
@@ -0,0 +1,51 @@
+/*
+* Copyright (c) 2014,北京航空航天大学
+* All rights reserved.
+*
+* 系统名称: SPACE NETWORK SIMULATION SYSTEM
+* 作    者: 张涛
+* 当前版本: SNS 2.0
+* 
+* 文件名称: Sim_Model.cpp
+* 当前版本: V 2.0.0
+* 文件标识: 工程运行入口main()函数
+* 功能描述: 为模型开发测试提供依附节点的支持
+* 完 成 人: 
+* 联系方式: 
+* 完成日期: 
+*
+* 摘    要:
+*/
+//////////////////////////////////////////////////////////////////////
+
+
+
+#include "stdafx.h"
+#include <malloc.h>
+#include <TIME.H>
+#include "Sns_Scenarios.h"
+
+static void Sns_main();
+
+int main(int argc, char* argv[])
+{
+	
+	Sns_main();
+
+	return 0;
+}
+
+void Sns_main()
+{
+	Sns_Scenarios *ScenariosData;
+	ScenariosData = new Sns_Scenarios();
+
+	printf("SNSim Init is beginning......\n");
+	ScenariosData->SNS_Init();			//运行环境初始化
+	printf("SNSim Running is beginning......\n");
+	ScenariosData->SNS_Simulate_Run();	//仿真事件处理函数
+	printf("SNSim Data record is beginning......\n");
+	ScenariosData->SNS_Simulate_End();	//仿真结束后相关动作处理
+	printf("SNSim is end.\n");
+	system("pause");
+}
---
+++ Sim_Model/StdAfx.h
@@ -0,0 +1,61 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__8A318802_D471_45A9_81F1_FD5B3038B00D__INCLUDED_)
+#define AFX_STDAFX_H__8A318802_D471_45A9_81F1_FD5B3038B00D__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <memory.h>
+//c++头文件
+#include <algorithm>//STL 通用算法   
+#include <bitset>//STL 位集容器   
+#include <cctype>   
+#include <cerrno>   
+#include <clocale>   
+#include <cmath>   
+#include <complex>//复数类   
+#include <cstdio>   
+#include <cstdlib>   
+#include <cstring>   
+#include <ctime>   
+#include <deque>//STL 双端队列容器   
+#include <exception>//异常处理类   
+#include <fstream>   
+#include <functional>//STL 定义运算函数（代替运算符）   
+#include <limits>   
+#include <list>//STL 线性列表容器   
+#include <map>//STL 映射容器   
+#include <iomanip>   
+#include <ios>//基本输入／输出支持   
+#include <iosfwd>//输入／输出系统使用的前置声明   
+#include <iostream>   
+#include <istream>//基本输入流   
+#include <ostream>//基本输出流   
+#include <queue>//STL 队列容器   
+#include <set>//STL 集合容器   
+#include <sstream>//基于字符串的流   
+#include <stack>//STL 堆栈容器　　　　   
+#include <stdexcept>//标准异常类   
+#include <streambuf>//底层输入／输出支持   
+#include <string>//字符串类   
+#include <utility>//STL 通用模板类   
+#include <vector>//STL 动态数组容器   
+#include <cwchar>   
+#include <cwctype>   
+using namespace std; 
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__8A318802_D471_45A9_81F1_FD5B3038B00D__INCLUDED_)
